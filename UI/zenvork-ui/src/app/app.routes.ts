86395fb78dd1dab343d5ec9c8ef7c25ae7a58ea4
Nikhil Raghuwanshi <nikhilofficial557@gmail.com>
2026-03-15 21:15:43 +0530

    Added sidenav and header main layout component

--- UI/zenvork-ui/src/app/app.routes.ts
+++ UI/zenvork-ui/src/app/app.routes.ts
@@ -1,19 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/pages/login/login.component';
+import { MainLayoutComponent } from './modules/layout/pages/main-layout/main-layout.component';
+import { HomeComponent } from './modules/home/pages/home.component';
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
-    {
-        path: "",
-        redirectTo: "login",
-        pathMatch: "full",
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./modules/auth/pages/login/login.component').then(m => m.LoginComponent)
-    },
-    {
+  // Public Route
+  {
+    path: 'login',
+    loadComponent: () => import('./modules/auth/pages/login/login.component').then(m => m.LoginComponent),
+  },
+
+  // Protected Routes with Layout
+  {
+    path: '',
+    loadComponent: () => import('./modules/layout/pages/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+    canActivate: [authGuard],
+    children: [
+      {
         path: 'home',
-        canActivate: [authGuard],
-        loadComponent: () => import('./modules/home/pages/home.component').then(m => m.HomeComponent)
-    }
+        loadComponent: () => import('./modules/home/pages/home.component').then(m => m.HomeComponent),
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
 ];
