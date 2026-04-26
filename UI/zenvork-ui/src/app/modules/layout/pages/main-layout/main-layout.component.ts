eb6a43995e63d33c0d6d3fa8e83c81dcbff0da18
Nikhil Raghuwanshi <nikhilofficial557@gmail.com>
2026-04-26 23:20:49 +0530

    Toolbar added

--- UI/zenvork-ui/src/app/modules/layout/pages/main-layout/main-layout.component.ts
+++ UI/zenvork-ui/src/app/modules/layout/pages/main-layout/main-layout.component.ts
@@ -1,19 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { HeaderComponent } from '../header/header.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [MatSidenavModule, HeaderComponent, SidebarComponent, RouterOutlet],
+  imports: [ToolbarComponent, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
 export class MainLayoutComponent {
-  @ViewChild('sidenav') sidenav!: MatSidenav;
-
-  toggleSidenav = () => {
-    this.sidenav.toggle();
-  };
 }
